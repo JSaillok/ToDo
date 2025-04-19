eb7e4bdccc2452f11e19e3c852eb484efb5fa172
JSaillok <johndday07@gmail.com>
2025-04-19 22:11:47 +0300

    Submit and stack items

--- react/src/components/Todo.jsx
+++ react/src/components/Todo.jsx
@@ -2,12 +2,24 @@ import { useState } from "react";
 
 export default function Todo() {
     const [todo, setTodo] = useState("");
+    const [todos, setTodos] =useState([]);
+    function handleSubmit(e) {
+        e.preventDefault();
+        setTodos([...todos,todo]);
+        //by this we can stack the todos.
+        setTodo(""); // clears input
+    }
     return(
         <div>
-            <form>
-                <input onChange={(e)=> setTodo(e.target.value)} value={todo} type="text" />
+            <form onSubmit={handleSubmit}>
+                <input
+                    onChange={(e)=> setTodo(e.target.value)}
+                    value={todo}
+                    type="text"
+                />
                 <button type="submit">Add</button>
             </form>
+            {console.log(todos)}
         </div>
     );
 }

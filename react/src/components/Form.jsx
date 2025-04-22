3ddde39e9d18e4d29358d4eb2277abe93f9f741a
Ioannis Kollias <johndday07@gmail.com>
2025-04-22 12:45:06 +0300

    style its component

--- react/src/components/Form.jsx
+++ react/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./form.module.css"
 
 export default function Form({todos, setTodos}) {
     const [todo, setTodo] = useState("");
@@ -8,13 +9,19 @@ export default function Form({todos, setTodos}) {
         setTodo("");
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                onChange={(e)=> setTodo(e.target.value)}
-                value={todo}
-                type="text"
-            />
-            <button type="submit">Add</button>
+        <form className={styles.todoform} onSubmit={handleSubmit}>
+            {/* the add button be on the right of input */}
+            <div className={styles.inputContainer}> 
+                <input className={styles.Input}
+                    onChange={(e)=> setTodo(e.target.value)}
+                    value={todo}
+                    type="text"
+                    placeholder="Enter todo item..."
+                />
+                <button className={styles.Button} type="submit">
+                    Add
+                </button>
+            </div>
         </form>
     );
 }

7da5790e6f996d234ed1fe5de8b3204e37eb46ad
Ioannis Kollias <johndday07@gmail.com>
2025-04-22 12:09:19 +0300

    Seperate components

--- react/src/components/Todo.jsx
+++ react/src/components/Todo.jsx
@@ -1,29 +1,13 @@
 import { useState } from "react";
-import TodoItem from "./TodoItem";
+import Form from "./Form"
+import TodoList from "./TodoList";
 
 export default function Todo() {
-    const [todo, setTodo] = useState("");
     const [todos, setTodos] =useState([]);
-    function handleSubmit(e) {
-        e.preventDefault();
-        setTodos([...todos,todo]);
-        setTodo("");
-    }
     return(
         <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    onChange={(e)=> setTodo(e.target.value)}
-                    value={todo}
-                    type="text"
-                />
-                <button type="submit">Add</button>
-            </form>
-            {/* {todos} in this way it shows the stacked todo items one next to other*/}
-            {todos.map(item=> (
-                //<h3>{item}</h3> by this occurs error about unique key
-                <TodoItem key={item} item={item}/>
-            ))}
+            <Form todos={todos} setTodos={setTodos}/>
+            <TodoList todos={todos} />
         </div>
     );
 }

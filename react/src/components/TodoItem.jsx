a95a1297e98443d10fb167176bdd7eb44e8f9719
Ioannis Kollias <johndday07@gmail.com>
2025-04-22 13:02:24 +0300

    handle deleted todos

--- react/src/components/TodoItem.jsx
+++ react/src/components/TodoItem.jsx
@@ -1,15 +1,28 @@
 import styles from "./todoitem.module.css"
 
-export default function TodoItem({item}){
+export default function TodoItem({item, todos, setTodos}){
+    function handleDelete() {
+        console.log("Delete button clicked for item", item);
+        // is similar to map()
+        //takes all list of todos, and calls back all except the item
+        setTodos(todos.filter((todo)=>todo !== item));
+    }
     return(
         <div className={styles.item}>
             <div className={styles.itemName}>
                 {item}
                 <span>
-                    <button className={styles.Button}>X</button>
+                    <button 
+                        onClick={()=>handleDelete(item)} 
+                        className={styles.Button}
+                    >
+                        X
+                    </button>
                 </span>
             </div>
             <hr className={styles.line} />
         </div>
     );
 }
+
+// the callback function is to prevent insta call the handleDelete item function.
--- react/src/components/TodoList.jsx
+++ react/src/components/TodoList.jsx
@@ -1,12 +1,15 @@
 import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css";
 
-export default function TodoList({todos}) {
+export default function TodoList({todos, setTodos}) {
     return(
         <div className={styles.list}>
             {todos.map(item=> (
-                <TodoItem key={item} item={item}/>
+                <TodoItem key={item} item={item} todos={todos} setTodos={setTodos}/>
             ))}
         </div>
     );
 }
+
+//we pass also todos so we know which one we have to delete
+// setTodos to pass the new list without the deleted item.

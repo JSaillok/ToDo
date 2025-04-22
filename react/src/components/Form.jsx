3b0468c0b41d0b406f73e04cf83dad6d86550c16
Ioannis Kollias <johndday07@gmail.com>
2025-04-22 22:16:14 +0300

    adding the done flag

--- react/src/components/Form.jsx
+++ react/src/components/Form.jsx
@@ -2,19 +2,21 @@ import { useState } from "react";
 import styles from "./form.module.css"
 
 export default function Form({todos, setTodos}) {
-    const [todo, setTodo] = useState("");
+    // const [todo, setTodo] = useState("");
+    const [todo, setTodo] = useState({name:"", done: false});
+    
     function handleSubmit(e) {
         e.preventDefault();
         setTodos([...todos,todo]);
-        setTodo("");
+        setTodo({name:"", done: false});
     }
     return (
         <form className={styles.todoform} onSubmit={handleSubmit}>
             {/* the add button be on the right of input */}
             <div className={styles.inputContainer}> 
                 <input className={styles.Input}
-                    onChange={(e)=> setTodo(e.target.value)}
-                    value={todo}
+                    onChange={(e)=> setTodo({name:e.target.value, done:false})}
+                    value={todo.name}
                     type="text"
                     placeholder="Enter todo item..."
                 />
